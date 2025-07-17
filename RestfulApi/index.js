52b8476dfcc504e0808743cf995515aeb8f38de0
Tanveer Alam <tanveeralam2123@gmail.com>
2025-07-17 12:35:51 +0530

    start database

--- RestfulApi/index.js
+++ RestfulApi/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
 const fs = require("fs");
+const { type } = require("os");
 
 const app = express();
 
@@ -22,13 +23,13 @@ app.get("/users", (req, res) => {
   const html = `<ul>
     ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
     </ul>`;
-  res.send(html);
+  res.status(200).send(html);
 });
 app.post("/api/users/signup", (req, res) => {
   const body = req.body;
   users.push({ ...body, id: users.length + 1 });
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
-    return res.json({ status: "success", id: users.length });
+    return res.status(201).json({ status: "success", id: users.length });
   });
 });
 
@@ -38,15 +39,47 @@ app
   .get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
+    if (!user) {
+      res.status(404).send({ req: "bad request" });
+    }
     return res.json(user);
   })
   .patch((req, res) => {
-    //edit user with id
-    return res.json({ status: "pending" });
+    const id = Number(req.params.id);
+    const body = req.body;
+    const userIndex = users.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+      return res
+        .status(404)
+        .json({ status: "error", message: "User not found" });
+    }
+    users[userIndex] = { ...users[userIndex], ...body };
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ status: "error", message: "Failed to update user" });
+      }
+      return res.json({ status: "success", user: users[userIndex] });
+    });
   })
   .delete((req, res) => {
-    //delete  user with id
-    return res.json({ status: "sucess" });
+    const id = Number(req.params.id);
+    const userIndex = users.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+      return res
+        .status(404)
+        .json({ status: "error", message: "User not found" });
+    }
+    users.splice(userIndex, 1);
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err) => {
+      if (err) {
+        return res
+          .status(500)
+          .json({ status: "error", message: "Failed to delete user" });
+      }
+      return res.json({ status: "success", message: "User deleted" });
+    });
   });
 
 const PORT = 8000;
